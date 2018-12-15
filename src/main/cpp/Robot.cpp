0e69b719677d6ad077c8552e03ff0ee4e5222034
CJBuchel <connorbuchel@gmail.com>
2018-12-15 14:49:30 +0800

    Something MEaningfull\

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -9,11 +9,15 @@ void Robot::RobotInit() {
   //  Setup The xbox controller (usage::xbox) and the drivetrain (usage::drivetrain)
   //    Try not to hard code the values (try using GetChannel on the motors and GetPort on the
   //    xbox controller)
+  usage::drivetrain(0, 1);
+  usage::xbox(0, true);
 }
 
 void Robot::RobotPeriodic() {
   // Group 1: 
   //  Use get_control_signal on the '_control' variable and pass it to the Drive() method.
+  std::pair<double, double> signal = _control.get_control_signal(_xbox);
+  Drive(signal.first, signal.second);
 }
 
 void Robot::Drive(double left, double right) {
